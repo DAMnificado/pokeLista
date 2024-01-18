8c220934963e6ace6566fee17ea96cf946431a96
Héctor A. Mascuñana Rodríguez <damnificado108@gmail.com>
2024-01-18 18:53:39 +0100

    erg

--- script.js
+++ script.js
@@ -1,68 +1,34 @@
 import Pokemon from './pokemon.js';
 
-
-
-const url = "https://pokeapi.co/api/v2/pokemon/"
+const url = "https://pokeapi.co/api/v2/pokemon/";
 
 let listaPokemon = [];
-document.addEventListener("DOMContentLoaded", () =>{
-   
 
+document.addEventListener("DOMContentLoaded", () => {
     let lista = document.getElementById("lista");
 
-
     fetchDataFromUrl(url)
-    .then((listaJson) => {
+        .then((listaJson) => {
+            let pokemonPromises = Object.values(listaJson.results).map((values, index) => {
+                let pokemon = new Pokemon();
+                pokemon.nombre = values.name;
 
+                return fetchDataFromUrl(values.url)
+                    .then((datosPokemon) => {
+                        pokemon.imagen = datosPokemon.sprites.front_default;
+                        return pokemon;
+                    });
+            });
 
-       
-        let pokemonPromises= Object.values(listaJson.results).forEach((values, index) => {
-
-    let pokemon = new Pokemon();
-
-    pokemon.nombre = values.name;
-
-   return fetchDataFromUrl(values.url)
-    .then((datosPokemon) => {
-
-
-    pokemon.imagen = datosPokemon.sprites.front_default;
-    return pokemon;
-       
-    });
-    
-    
-
-   });
-
-   return Promise.all(pokemonPromises);
-   
-
- 
-
-    }).then(pokemones =>{
-
-        listaPokemon = pokemones; 
-    console.log(listaPokemon);
-
-
-    })
-
-
-
-
-
+            return Promise.all(pokemonPromises);
+        })
+        .then((pokemones) => {
+            listaPokemon = pokemones;
+            console.log(listaPokemon);
+        });
 });
 
-
-
-
-function fetchDataFromUrl(url){
-
+function fetchDataFromUrl(url) {
     return fetch(url)
-
-    // resolve
-    .then( (response) => response.json());
-    // reject
-
+        .then((response) => response.json());
 }
