4306119396eabd2a7adbfaeeea1b4c0d132df4d9
Héctor A. Mascuñana Rodríguez <damnificado108@gmail.com>
2024-01-18 18:16:45 +0100

    vinagre

---
+++ pokemon.js
@@ -0,0 +1,10 @@
+class Pokemon {
+
+    constructor(nombre, imagen) {
+        this.nombre = nombre;
+        this.imagen = imagen;
+    }
+
+}
+
+export default Pokemon;
---
+++ script.js
@@ -0,0 +1,67 @@
+import Pokemon from './pokemon.js';
+
+listaPokemon = [];
+
+const url = "https://pokeapi.co/api/v2/pokemon/"
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+
+    let lista = document.getElementById("lista");
+
+
+    fetchDataFromUrl(url)
+    .then((listaJson) => {
+
+
+
+   Object.values(listaJson.results).forEach((values, index) => {
+
+    pokemon = new Pokemon();
+
+    pokemon.nombre = values.name;
+
+    fetchDataFromUrl(values.url)
+    .then((datosPokemon) => {
+
+
+    pokemon.imagen = datosPokemon.sprites.front_default;
+    return pokemon;
+       
+    }).then((pokemon) => {
+
+        listaPokemon.push(pokemon);
+
+
+
+
+
+    });
+
+    
+    
+
+   });
+
+   console.log(listaPokemon);
+
+    });
+
+
+
+
+
+});
+
+
+
+
+function fetchDataFromUrl(url){
+
+    return fetch(url)
+
+    // resolve
+    .then( (response) => response.json());
+    // reject
+
+}
